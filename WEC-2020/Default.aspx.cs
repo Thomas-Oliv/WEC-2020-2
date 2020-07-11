0551a80f122bcf9ed6a05b27d94575f931358ab4
rlevesqu <rlevesqu@uwaterloo.ca>
2020-07-11 07:54:16 -0700

    search function edit

--- WEC-2020/Default.aspx.cs
+++ WEC-2020/Default.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -22,6 +23,14 @@ namespace WEC_2020
                 string api = "https://www.googleapis.com/customsearch/v1?key=AIzaSyAGNAnJ5faOpyrvkgc4pHkSqQhSzenrlUc&cx=012776195944492167572:vadbep5zpaq&q=";
                 string q = SearchQuery.Text.Replace(' ', '+');
                 api += q;
+                try
+                {
+                    var json = new WebClient().DownloadString(api);
+                }
+                catch
+                {
+
+                }
                 // Perform Search method on SearchQuery.Text
             }
         }
